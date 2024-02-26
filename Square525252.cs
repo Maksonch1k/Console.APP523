d98492db12970cbd7579063b9f47537596711fe1
Maksonch1k <123887520+Maksonch1k@users.noreply.github.com>
2024-02-26 09:48:27 +0200

    Add files via upload

---
+++ Square525252.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Уч__пр_3
+{
+    class Square:Figure
+    {
+        private int side1;
+
+        public Square(string name, int side1) : base(name)
+        {
+            this.side1 = side1;
+        }
+        public override double Area()
+        {
+            return Math.Pow(side1, 2);
+        }
+    }
+}
